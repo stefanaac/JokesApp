1324c5ae956a3e56423cd81a59939baac9d8c346
stefanaac <stefanac288@gmail.com>
2022-03-27 19:51:05 +0300

    Initial commit

---
+++ Jokes Web App/Models/Joke.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jokes_Web_App.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public string Question { get; set; }
+        public string Answear { get; set; }
+
+        public Joke()
+        {
+
+        }
+
+
+
+    }
+}
